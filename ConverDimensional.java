b01536266e40988f8375133c9555f48a39eff02c
lamabeta <148013077+lamabeta@users.noreply.github.com>
2023-12-20 17:33:46 +0300

    Create ConverDimensional.java

---
+++ ConverDimensional.java
@@ -0,0 +1,9 @@
+public class ConverDimensional {
+   public static int[][] conver(int[] nums) {
+              /*add your code */
+   }
+   public static void main(String[] args) {
+       int[] nums = {2,9,5,3,8,1,4,6,7};
+      /*call the method and print array here*/
+   }
+}
